a8d18b29b632d42a80be5fbd4cabb4466d1a50d2
Snackhole <>
2019-09-18 17:28:26 -0500

    Clear out old files

--- Source/Utility.py
+++
@@ -1,7 +0,0 @@
-def GetSafeFileNameFromPageTitle(PageTitle):
-    SafeFileName = PageTitle.replace("/", " - ")
-    SafeFileName = SafeFileName.replace("\\", " - ")
-    SafeFileName = SafeFileName.replace(":", " - ")
-    SafeFileName = SafeFileName.replace("|", " - ")
-    SafeFileName = "".join([Character for Character in SafeFileName if Character.isalpha() or Character.isdigit() or Character in (" ", ".", "_", "-")])
-    return SafeFileName

7d8f9db5382b5445575803ff07aff719d42404cf
Snackhole <37544796+Snackhole@users.noreply.github.com>
2019-10-21 09:24:56 -0500

    Start work on 22

--- Build.py
+++ Build.py
@@ -6,7 +6,7 @@ from PyInstaller import __main__ as PyInstall
 
 def Build():
     # Version String
-    Version = "21"
+    Version = "22"
 
     # Build Variables
     ExecutableScript = "Source/SerpentNotes " + Version + ".pyw"

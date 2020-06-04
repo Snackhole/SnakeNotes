51b5ec5c7a8d0e33f73be4f95b45866de78df728
Snackhole <>
2020-06-04 16:15:39 -0500

    Build script written

--- Build.py
+++ Build.py
@@ -1,93 +1,70 @@
+import os
+import shutil
+import zipapp
+
 # Build Variables
 Version = "30"
 AppName = "SerpentNotes"
 VersionedAppName = AppName + " " + Version
 
-# import os
-# import shutil
-#
-# from PyInstaller import __main__ as PyInstall
-#
-# # Built with Python 3.6.3 and dependencies in requirements.txt
-#
-# def Build():
-#     # Version String
-#     Version = "29"
-#
-#     # Build Variables
-#     ExecutableScript = "Source/SerpentNotes " + Version + ".pyw"
-#     ExecutableZip = "Executables/Final/" + ExecutableScript[7:-4] + ".zip"
-#     InstallerScript = "SerpentNotes " + Version + " Installer.pyw"
-#
-#     # Build Executable
-#     PyInstall.run(pyi_args=[ExecutableScript,
-#                             "--clean",
-#                             "--windowed",
-#                             "--name=SerpentNotes",
-#                             "--icon=Source/Assets/SerpentNotes Icon.ico",
-#                             "--add-data=Source/Assets/SerpentNotes Icon.ico;./Assets/",
-#                             "--add-data=Source/Assets/SerpentNotes Bold Icon.png;./Assets/",
-#                             "--add-data=Source/Assets/SerpentNotes Back Icon.png;./Assets/",
-#                             "--add-data=Source/Assets/SerpentNotes Forward Icon.png;./Assets/",
-#                             "--add-data=Source/Assets/SerpentNotes Bullet List Icon.png;./Assets/",
-#                             "--add-data=Source/Assets/SerpentNotes Delete Page Icon.png;./Assets/",
-#                             "--add-data=Source/Assets/SerpentNotes Demote Page Icon.png;./Assets/",
-#                             "--add-data=Source/Assets/SerpentNotes Icon.png;./Assets/",
-#                             "--add-data=Source/Assets/SerpentNotes Insert External Link Icon.png;./Assets/",
-#                             "--add-data=Source/Assets/SerpentNotes Insert Link(s) Icon.png;./Assets/",
-#                             "--add-data=Source/Assets/SerpentNotes Insert Table Icon.png;./Assets/",
-#                             "--add-data=Source/Assets/SerpentNotes Insert Image Icon.png;./Assets/",
-#                             "--add-data=Source/Assets/SerpentNotes Italics Icon.png;./Assets/",
-#                             "--add-data=Source/Assets/SerpentNotes Move Page Down Icon.png;./Assets/",
-#                             "--add-data=Source/Assets/SerpentNotes Move Page Up Icon.png;./Assets/",
-#                             "--add-data=Source/Assets/SerpentNotes New Page Icon.png;./Assets/",
-#                             "--add-data=Source/Assets/SerpentNotes Number List Icon.png;./Assets/",
-#                             "--add-data=Source/Assets/SerpentNotes Promote Page Icon.png;./Assets/",
-#                             "--add-data=Source/Assets/SerpentNotes Quote Icon.png;./Assets/",
-#                             "--add-data=Source/Assets/SerpentNotes Rename Page Icon.png;./Assets/",
-#                             "--add-data=Source/Assets/SerpentNotes Strikethrough Icon.png;./Assets/",
-#                             "--add-data=Source/Assets/SerpentNotes Toggle Read Mode Icon.png;./Assets/",
-#                             "--add-data=Source/Assets/SerpentNotes Zoom In Icon.png;./Assets/",
-#                             "--add-data=Source/Assets/SerpentNotes Zoom Out Icon.png;./Assets/",
-#                             "--add-data=Source/Assets/SerpentNotes Favorites Icon.png;./Assets/",
-#                             "--add-data=Source/Assets/SerpentNotes Search Icon.png;./Assets/",
-#                             "--add-data=Source/Assets/SerpentNotes Toggle Search Icon.png;./Assets/",
-#                             "--workpath=./Executables/Build",
-#                             "--distpath=./Executables/Final"])
-#
-#     # Zip Executable
-#     shutil.make_archive(ExecutableZip[:-4], "zip", "Executables/Final/SerpentNotes")
-#
-#     # Build Installer
-#     PyInstall.run(pyi_args=[InstallerScript,
-#                             "--clean",
-#                             "--windowed",
-#                             "--onefile",
-#                             "--name=SerpentNotes " + Version + " Installer",
-#                             "--icon=Source/Assets/SerpentNotes Icon.ico",
-#                             "--add-data=Source/Assets/SerpentNotes Icon.png;./Source/Assets/",
-#                             "--add-binary=./" + ExecutableZip + ";./Executables/Final",
-#                             "--workpath=./Installer/Build",
-#                             "--distpath=./Installer/Final"])
-#
-#     # Move Files to Versions Folder
-#     VersionsSubFolder = os.path.dirname("Versions/SerpentNotes " + Version + "/")
-#     if not os.path.exists(VersionsSubFolder):
-#         os.makedirs(VersionsSubFolder)
-#     shutil.copy(ExecutableZip, VersionsSubFolder)
-#     shutil.copy("Installer/Final/SerpentNotes " + Version + " Installer.exe", VersionsSubFolder)
-#
-#     # Delete Build Files
-#     for Folder in ["Executables/", "Installer/"]:
-#         shutil.rmtree(Folder, True)
-#     for File in os.listdir("."):
-#         if File.endswith(".spec"):
-#             os.unlink(File)
-#
-#     # Mark Source Files as Built
-#     os.rename(ExecutableScript, "Source/BUILT SerpentNotes " + Version + ".pyw")
-#     os.rename(InstallerScript, "BUILT " + InstallerScript)
-#
-#
-# if __name__ == "__main__":
-#     Build()
+CodeFiles = ["Core", "Interface", "SaveAndLoad", "Build.py", "SerpentNotes.py"]
+AssetFiles = ["Assets"]
+
+ExecutableZipName = AppName + ".pyzw"
+Interpreter = "python3"
+Main = AppName + ":StartApp"
+
+
+def Build():
+    # Additional Build Variables
+    BuildFolder = "BUILD - " + VersionedAppName
+
+    # Build Functions
+    def CopyFilesToBuildFolder(CopiedFiles):
+        IgnoredFiles = [File for File in os.listdir(".") if File not in CopiedFiles]
+        shutil.copytree(".", BuildFolder, ignore=lambda Source, Contents: IgnoredFiles)
+
+    def CleanUp():
+        shutil.rmtree(BuildFolder)
+        print("Build files cleaned up.")
+
+    # Copy Code to Build Folder
+    CopyFilesToBuildFolder(CodeFiles)
+    print("Code files copied to build folder.")
+
+    # Create Executable Archive
+    zipapp.create_archive(BuildFolder, ExecutableZipName, Interpreter, Main)
+    print("Executable archive created.")
+
+    # Delete Build Folder
+    shutil.rmtree(BuildFolder)
+    print("Build folder deleted.")
+
+    # Copy Assets to Build Folder and Move Executable Zip
+    CopyFilesToBuildFolder(AssetFiles)
+    print("Assets copied to build folder.")
+    shutil.move(ExecutableZipName, BuildFolder)
+    print("Executable archive moved to build folder.")
+
+    # Prompt to Install Dependencies
+    ProceedPrompt = "\n---\nInstall dependencies to build folder (" + BuildFolder + ") using a command prompt:\n\n    python -m pip install -r \"" + os.getcwd() + "\\requirements.txt\" --target \"" + os.getcwd() + "\\" + BuildFolder + "\"\n\nOnce all dependencies are installed, input \"PROCEED\" to continue with build or \"CANCEL\" to cancel and clean up build files:\n---\n"
+    ProceedResponse = input(ProceedPrompt)
+    if ProceedResponse == "PROCEED":
+        pass
+    elif ProceedResponse == "CANCEL":
+        print("Build canceled.")
+        CleanUp()
+        return
+    else:
+        return
+
+    # Zip Build
+    shutil.make_archive(VersionedAppName, "zip", BuildFolder)
+    print("Build zipped.")
+
+    # Clean Up
+    CleanUp()
+
+
+if __name__ == "__main__":
+    Build()

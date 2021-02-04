23b55521584b6cc2789f7c6a77f3905f126ea77d
Snackhole <37544796+Snackhole@users.noreply.github.com>
2021-02-04 15:43:04 -0600

    Added searching to insert image dialog

--- Interface/Dialogs/InsertImageDialog.py
+++ Interface/Dialogs/InsertImageDialog.py
@@ -1,5 +1,6 @@
+from PyQt5 import QtCore
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QDialog, QGridLayout, QLabel, QListWidget, QListWidgetItem, QPushButton, QScrollArea, QSplitter
+from PyQt5.QtWidgets import QCheckBox, QDialog, QGridLayout, QLabel, QLineEdit, QListWidget, QListWidgetItem, QPushButton, QScrollArea, QSplitter
 
 from Core import Base64Converters
 
@@ -18,8 +19,18 @@ class InsertImageDialog(QDialog):
         self.Width = max(self.MainWindow.width() - 100, 100)
         self.Height = max(self.MainWindow.height() - 100, 100)
 
+        # Search Line Edit
+        self.SearchLineEdit = SearchLineEdit(self)
+        self.SearchLineEdit.setPlaceholderText("Search")
+        self.SearchLineEdit.textChanged.connect(self.PopulateImageList)
+        self.SearchLineEdit.setFocus()
+
+        # Match Case Check Box
+        self.MatchCaseCheckBox = QCheckBox("Match Case")
+        self.MatchCaseCheckBox.stateChanged.connect(self.PopulateImageList)
+
         # Image List
-        self.ImageList = QListWidget()
+        self.ImageList = ImageList(self)
         self.ImageList.itemSelectionChanged.connect(self.ItemSelected)
 
         # Image Display
@@ -32,21 +43,22 @@ class InsertImageDialog(QDialog):
         self.CancelButton.clicked.connect(self.Cancel)
 
         # Create, Populate, and Set Layout
-        self.ImageDisplayScrollArea = QScrollArea()
-        self.ImageDisplayScrollArea.setWidget(self.ImageDisplay)
-
+        self.Layout = QGridLayout()
+        self.SearchLayout = QGridLayout()
+        self.SearchLayout.addWidget(self.SearchLineEdit, 0, 0)
+        self.SearchLayout.addWidget(self.MatchCaseCheckBox, 0, 1)
+        self.Layout.addLayout(self.SearchLayout, 0, 0)
         self.Splitter = QSplitter()
         self.Splitter.addWidget(self.ImageList)
+        self.ImageDisplayScrollArea = QScrollArea()
+        self.ImageDisplayScrollArea.setWidget(self.ImageDisplay)
         self.Splitter.addWidget(self.ImageDisplayScrollArea)
         self.Splitter.setStretchFactor(1, 1)
-
+        self.Layout.addWidget(self.Splitter, 1, 0)
         self.ButtonLayout = QGridLayout()
         self.ButtonLayout.addWidget(self.InsertImageButton, 0, 0)
         self.ButtonLayout.addWidget(self.CancelButton, 0, 1)
-
-        self.Layout = QGridLayout()
-        self.Layout.addWidget(self.Splitter, 0, 0)
-        self.Layout.addLayout(self.ButtonLayout, 1, 0)
+        self.Layout.addLayout(self.ButtonLayout, 2, 0)
         self.setLayout(self.Layout)
 
         # Set Window Title and Icon
@@ -73,12 +85,18 @@ class InsertImageDialog(QDialog):
             self.ImageDisplay.resize(self.ImageDisplay.pixmap().size())
 
     def PopulateImageList(self):
+        SearchTerm = self.SearchLineEdit.text()
+        MatchCase = self.MatchCaseCheckBox.isChecked()
+        if not MatchCase:
+            SearchTerm = SearchTerm.lower()
         self.ImageList.clear()
         self.ImageDisplay.clear()
-        for FileName, Base64String in sorted(self.Notebook.Images.items(), key=lambda Image: Image[0].lower()):
+        Images = sorted(self.Notebook.Images.items(), key=lambda Image: Image[0].lower())
+        if SearchTerm != "":
+            Images = [Image for Image in Images if SearchTerm in (Image[0].lower() if not MatchCase else Image[0])]
+        for FileName, Base64String in Images:
             self.ImageList.addItem(ImageListItem(FileName, Base64String))
         self.ImageList.setCurrentRow(0)
-        self.ImageList.setFocus()
 
     def InsertImage(self):
         SelectedItems = self.ImageList.selectedItems()
@@ -103,3 +121,36 @@ class ImageListItem(QListWidgetItem):
         self.Base64String = Base64String
 
         self.setText(self.FileName)
+
+
+class SearchLineEdit(QLineEdit):
+    def __init__(self, Dialog):
+        # QLineEdit Init
+        super().__init__()
+
+        # Store Parameters
+        self.Dialog = Dialog
+
+    def keyPressEvent(self, QKeyEvent):
+        KeyPressed = QKeyEvent.key()
+        if KeyPressed == QtCore.Qt.Key_Down:
+            self.Dialog.ImageList.setFocus()
+        else:
+            super().keyPressEvent(QKeyEvent)
+
+
+class ImageList(QListWidget):
+    def __init__(self, Dialog):
+        # QListWidget Init
+        super().__init__()
+
+        # Store Parameters
+        self.Dialog = Dialog
+
+    def keyPressEvent(self, QKeyEvent):
+        KeyPressed = QKeyEvent.key()
+        Row = self.currentRow()
+        if KeyPressed == QtCore.Qt.Key_Up and Row == 0:
+            self.Dialog.SearchLineEdit.setFocus()
+        else:
+            super().keyPressEvent(QKeyEvent)

3ad5e120eba306ad241c8b930aa3cc9a53d4b9aa
Snackhole <snackhole.dev@gmail.com>
2022-08-05 12:21:35 -0500

    Bump to 59

--- Build.py
+++ Build.py
@@ -7,7 +7,7 @@ import zipapp
 
 # Build Variables
 BuildVariables = {}
-BuildVariables["Version"] = "58"
+BuildVariables["Version"] = "59"
 BuildVariables["AppName"] = "SnakeNotes"
 BuildVariables["VersionedAppName"] = BuildVariables["AppName"] + " " + BuildVariables["Version"]
 

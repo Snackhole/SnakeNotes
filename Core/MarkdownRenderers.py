b8f2acd1314edab42b05641abdf795fe7a0f6906
Snackhole <37544796+Snackhole@users.noreply.github.com>
2024-09-15 17:50:50 -0500

    Replaced concatenation with f-strings in MarkdownRenderers

--- Core/MarkdownRenderers.py
+++ Core/MarkdownRenderers.py
@@ -14,49 +14,51 @@ class Renderer(mistune.Renderer):
     def link(self, Link, Title, Text):
         Link = mistune.escape_link(Link)
         if Link.startswith("[0,") and not self.Notebook.StringIsValidIndexPath(Link):
-            return Text + " (LINKED PAGE NOT FOUND)"
+            return f"{Text} (LINKED PAGE NOT FOUND)"
         if Link == "[deleted]":
-            return Text + " (LINKED PAGE DELETED)"
+            return f"{Text} (LINKED PAGE DELETED)"
         if not Title:
-            return "<a href=\"" + Link + "\">" + Text + "</a>"
+            return f"<a href=\"{Link}\">{Text}</a>"
         Title = mistune.escape(Title, quote=True)
-        return "<a href=\"" + Link + "\" title=\"" + Title + "\">" + Text + "</a>"
+        return f"<a href=\"{Link}\" title=\"{Title}\">{Text}</a>"
 
     def strikethrough(self, Text):
-        return "<s>" + Text + "</s>"
+        return f"<s>{Text}</s>"
 
     def footnote_ref(self, Key, Index):
         Key = mistune.escape(Key)
-        return "<sup class=\"footnote-ref\" id=\"fnref-" + Key + "\">" + Key + "</sup>"
+        return f"<sup class=\"footnote-ref\" id=\"fnref-{Key}\">{Key}</sup>"
 
     def footnote_item(self, Key, Text):
         Key = mistune.escape(Key)
-        Text = "<p>" + Key + ". " + Text.rstrip()[3:]
-        return "<li>" + Text + "</li>\n"
+        Text = f"<p>{Key}. {Text.rstrip()[3:]}"
+        return f"<li>{Text}</li>\n"
 
     def footnotes(self, Text):
-        return "<div class=\"footnotes\">\n" + self.hrule() + "<ul style=\"list-style-type:none\">" + Text + "</ul>\n</div>\n"
+        return f"<div class=\"footnotes\">\n{self.hrule()}<ul style=\"list-style-type:none\">{Text}</ul>\n</div>\n"
 
     def table(self, Header, Body):
-        return "<p><table border=\"1\" cellpadding=\"2\">\n<thead>" + Header + "</thead>\n<tbody>\n" + Body + "</tbody>\n</table>\n"
+        return f"<p><table border=\"1\" cellpadding=\"2\">\n<thead>{Header}</thead>\n<tbody>\n{Body}</tbody>\n</table>\n"
 
     def header(self, text, level, raw=None):
-        return "<h" + str(level) + " style=\"color: seagreen\">" + text + "</h" + str(level) + ">\n"
+        return f"<h{str(level)} style=\"color: seagreen\">{text}</h{str(level)}>\n"
 
     def image(self, Source, Title, AltText):
         if self.Notebook.HasImage(Source):
-            Source = "data:image/" + os.path.splitext(Source)[1] + ";base64, " + self.Notebook.GetImage(Source)
+            Source = f"data:image/{os.path.splitext(Source)[1]};base64, {self.Notebook.GetImage(Source)}"
             AltText = mistune.escape(AltText, quote=True)
             if Title is not None:
                 Title = mistune.escape(Title, quote=True)
-                HTML = "<img src=\"" + Source + "\" alt=\"" + AltText + "\" title=\"" + Title + "\""
+                HTML = f"<img src=\"{Source}\" alt=\"{AltText}\" title=\"{Title}\""
             else:
-                HTML = "<img src=\"" + Source + "\" alt=\"" + AltText + "\""
+                HTML = f"<img src=\"{Source}\" alt=\"{AltText}\""
             if self.options.get("use_xhtml"):
-                return HTML + " />"
-            return HTML + ">"
+                return f"{HTML} />"
+            return f"{HTML}>"
         else:
-            return "IMAGE NOT FOUND|" + Source + (("|" + AltText) if AltText != "" else "") + (("|" + Title) if Title is not None and Title != "" else "")
+            AltTextString = (f"|{AltText}") if AltText != "" else ""
+            TitleString = (f"|{Title}") if Title is not None and Title != "" else ""
+            return f"IMAGE NOT FOUND|{Source}{AltTextString}{TitleString}"
 
 
 class HTMLExportRenderer(Renderer):
@@ -67,17 +69,17 @@ class HTMLExportRenderer(Renderer):
         Link = mistune.escape_link(Link)
         ValidIndexPath = self.Notebook.StringIsValidIndexPath(Link)
         if Link.startswith("[0,") and not ValidIndexPath:
-            return Text + " (LINKED PAGE NOT FOUND)"
+            return f"{Text} (LINKED PAGE NOT FOUND)"
         if Title:
             Title = mistune.escape(Title, quote=True)
         if ValidIndexPath:
             if not Title:
-                return "<a href=\"\" onclick=\"return SelectPage(&quot;" + Link + "&quot;);\">" + Text + "</a>"
-            return "<a href=\"\" onclick=\"return SelectPage(&quot;" + Link + "&quot;);\" title=\"" + Title + "\">" + Text + "</a>"
+                return f"<a href=\"\" onclick=\"return SelectPage(&quot;{Link}&quot;);\">{Text}</a>"
+            return f"<a href=\"\" onclick=\"return SelectPage(&quot;{Link}&quot;);\" title=\"{Title}\">{Text}</a>"
         else:
             if not Title:
-                return "<a href=\"" + Link + "\" target=\"_blank\">" + Text + "</a>"
-            return "<a href=\"" + Link + "\" title=\"" + Title + "\" target=\"_blank\">" + Text + "</a>"
+                return f"<a href=\"{Link}\" target=\"_blank\">{Text}</a>"
+            return f"<a href=\"{Link}\" title=\"{Title}\" target=\"_blank\">{Text}</a>"
 
 
 class PDFExportRenderer(Renderer):
@@ -89,17 +91,17 @@ class PDFExportRenderer(Renderer):
 
 
 def ConstructMarkdownStringFromPage(Page, Notebook):
-    HeaderString = Notebook.Header + "\n\n"
+    HeaderString = f"{Notebook.Header}\n\n"
     HeaderString = HeaderString.replace("{PAGETITLE}", Page["Title"])
     HeaderString = HeaderString.replace("{SUBPAGELINKS}", ConstructSubPageLinks(Page))
     HeaderString = HeaderString.replace("{SUBPAGEOFLINK}", ConstructSubPageOfLink(Page, Notebook))
     HeaderString = HeaderString.replace("{LINKINGPAGES}", ConstructLinkingPagesLinks(Page, Notebook))
-    FooterString = "\n\n" + Notebook.Footer
+    FooterString = f"\n\n{Notebook.Footer}"
     FooterString = FooterString.replace("{PAGETITLE}", Page["Title"])
     FooterString = FooterString.replace("{SUBPAGELINKS}", ConstructSubPageLinks(Page))
     FooterString = FooterString.replace("{SUBPAGEOFLINK}", ConstructSubPageOfLink(Page, Notebook))
     FooterString = FooterString.replace("{LINKINGPAGES}", ConstructLinkingPagesLinks(Page, Notebook))
-    MarkdownString = HeaderString + Page["Content"] + FooterString
+    MarkdownString = f"{HeaderString}{Page["Content"]}{FooterString}"
     return MarkdownString
 
 
@@ -109,7 +111,7 @@ def ConstructSubPageLinks(Page):
     else:
         LinksString = ""
         for SubPage in Page["SubPages"]:
-            LinksString += "[" + SubPage["Title"] + "](" + json.dumps(SubPage["IndexPath"]) + ")  \n"
+            LinksString += f"[{SubPage["Title"]}]({json.dumps(SubPage["IndexPath"])})  \n"
         LinksString = LinksString.rstrip()
     return LinksString
 
@@ -119,18 +121,18 @@ def ConstructSubPageOfLink(Page, Notebook):
         LinkString = "This is the root page."
     else:
         SuperPage = Notebook.GetSuperOfPageFromIndexPath(Page["IndexPath"])
-        LinkString = "[" + SuperPage["Title"] + "](" + json.dumps(SuperPage["IndexPath"]) + ")"
+        LinkString = f"[{SuperPage["Title"]}]({json.dumps(SuperPage["IndexPath"])})"
     return LinkString
 
 
 def ConstructLinkingPagesLinks(Page, Notebook):
-    SearchResults = Notebook.GetSearchResults("](" + json.dumps(Page["IndexPath"]))
+    SearchResults = Notebook.GetSearchResults(f"]({json.dumps(Page["IndexPath"])}")
     if len(SearchResults["ResultsList"]) < 1:
         LinksString = "No linking pages."
     else:
         LinksString = ""
         for Result in SearchResults["ResultsList"]:
-            LinksString += "[" + Result[0] + "](" + json.dumps(Result[1]) + ")  \n"
+            LinksString += f"[{Result[0]}]({json.dumps(Result[1])})  \n"
         LinksString = LinksString.rstrip()
     return LinksString
 
@@ -159,12 +161,12 @@ def ConstructHTMLExportString(Notebook, AssetPaths):
 
 def GeneratePageListItemHTML(Page, Root=False):
     if len(Page["SubPages"]) > 0:
-        ListItemString = "<li class=\"PageListCollapsibleItem\"><input type=\"checkbox\" id=\"" + str(Page["IndexPath"]) + "checkbox\"" + " class=\"PageListCollapsibleCheckbox\"" + (" checked=\"checked\"" if Root else "") + "><label for=\"" + str(Page["IndexPath"]) + "checkbox\"></label><span id=\"" + str(Page["IndexPath"]) + "link\" class=\"PageLink\" onclick=\"SelectPage(&quot;" + str(Page["IndexPath"]) + "&quot;);\" ondblclick=\"TogglePageExpansion(&quot;" + str(Page["IndexPath"]) + "&quot;);\">" + Page["Title"] + "</span><ul>"
+        ListItemString = f"<li class=\"PageListCollapsibleItem\"><input type=\"checkbox\" id=\"{str(Page["IndexPath"])}checkbox\" class=\"PageListCollapsibleCheckbox\"{" checked=\"checked\"" if Root else ""}><label for=\"{str(Page["IndexPath"])}checkbox\"></label><span id=\"{str(Page["IndexPath"])}link\" class=\"PageLink\" onclick=\"SelectPage(&quot;{str(Page["IndexPath"])}&quot;);\" ondblclick=\"TogglePageExpansion(&quot;{str(Page["IndexPath"])}&quot;);\">{Page["Title"]}</span><ul>"
         for SubPage in Page["SubPages"]:
             ListItemString += GeneratePageListItemHTML(SubPage)
         ListItemString += "</ul></li>"
     else:
-        ListItemString = "<li><span id=\"" + str(Page["IndexPath"]) + "link\" class=\"PageLink\" onclick=\"SelectPage(&quot;" + str(Page["IndexPath"]) + "&quot;);\">" + Page["Title"] + "</span></li>"
+        ListItemString = f"<li><span id=\"{str(Page["IndexPath"])}link\" class=\"PageLink\" onclick=\"SelectPage(&quot;{str(Page["IndexPath"])}&quot;);\">{Page["Title"]}</span></li>"
     return ListItemString
 
 
